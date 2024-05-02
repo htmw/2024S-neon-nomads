7f386dad4f32384566cb95d4c4e48fc0db21e211
Shashank Akavaram <sa66081n@pace.edu>
2024-05-01 20:30:32 -0400

    Code with accurate nutri info and youtube videos

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from datetime import datetime, timedelta
 
 from datetime import date
 from flask import jsonify
@@ -33,6 +34,8 @@ migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['UPLOAD_FOLDER'] ="./static/predictimgs"
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -110,6 +113,61 @@ def prepare_image(img_path):
     x = np.expand_dims(x, axis=0)
     return x
 
+#caution code start
+FOOD_RECIPES_FILE_PATH = "static/food_list/Food_recipes_kitchensync.csv" 
+
+def get_recipe_recommendations(expiring_items):
+    # Read the Excel file into a DataFrame
+    recipes_df = pd.read_csv(FOOD_RECIPES_FILE_PATH)
+
+    # Initialize an empty list to store recommended recipes
+    recommended_recipes = []
+
+    # Iterate over each expiring item
+    for item in expiring_items:
+        # Get recipes that contain the expiring item as an ingredient
+        matching_recipes = recipes_df[recipes_df['Ingredients'].str.contains(item)]
+
+        # If there are matching recipes, add them to the recommended recipes list
+        if not matching_recipes.empty:
+            for _, recipe in matching_recipes.iterrows():
+                # Check if the recipe is already in the recommended recipes list
+                if recipe.Title not in [rec['title'] for rec in recommended_recipes]:
+                    # Append the title, ingredients, and instructions to the recommended recipes list
+                    recommended_recipes.append({
+                        'title': recipe.Title,
+                        'ingredients': recipe.Ingredients,
+                        'instructions': recipe.Instructions
+                    })
+
+    # Return the list of recommended recipes, limited to 5 recipes
+    return recommended_recipes[:5]
+
+
+
+def get_expiring_items(items):
+    print("Items:", items)  # Add this line for debugging
+
+    # Get the current date
+    current_date = datetime.now().date()
+
+    # Calculate the number of days until expiry for each item
+    for item in items:
+        item.days_until_expiry = (item.expiry_date - current_date).days
+
+    # Sort the items by days until expiry in ascending order
+    sorted_items = sorted(items, key=lambda x: x.days_until_expiry)
+
+    # Get the top 3 expiring items
+    expiring_items = sorted_items[:3]
+
+    # Extract the names of the expiring items
+    expiring_item_names = [item.item_name for item in expiring_items]
+
+    return expiring_item_names
+
+#caution code end
+
 
 @app.route('/')
 def home():
@@ -141,8 +199,62 @@ def dashboard():
         flash('Item added successfully!', 'success')
         return redirect(url_for('dashboard'))
     else:
+        # items = Inventory.query.filter_by(user_id=current_user.id).all()
+        # expiring_items = []
+        # for item in items:
+        #     days_until_expiry = (item.expiry_date - datetime.now().date()).days
+        #     expiring_items.append((item.item_name, days_until_expiry))
+        # # Sort expiring_items based on days_until_expiry
+        # expiring_items.sort(key=lambda x: x[1])
+        # # Take the top 3 items
+        # expiring_items = expiring_items[:3]
+
+        # Get items from inventory
         items = Inventory.query.filter_by(user_id=current_user.id).all()
-        return render_template('dashboard.html', items=items)
+
+        # Get top 3 expiring items
+        expiring_items = get_expiring_items(items)
+
+        # Get recommended recipes based on expiring items
+        recommended_recipes = get_recipe_recommendations(expiring_items)
+
+        return render_template('dashboard.html', items=items, expiring_items=expiring_items, recommended_recipes=recommended_recipes)
+
+        # return render_template('dashboard.html', items=items, expiring_items=expiring_items)
+
+
+# New route and function to fetch expiring items information
+@app.route('/fetch_expiring_items_info', methods=['POST'])
+def fetch_expiring_items_info():
+    # Get expiring items from the request
+    expiring_items = request.json.get('expiring_items', [])
+
+    # Initialize an empty list to store expiring items information
+    expiring_items_info = []
+
+    # Get the current date
+    current_date = datetime.now().date()
+
+    # Calculate the number of days until expiry for each item
+    for item in expiring_items:
+        # Convert item to string
+        item_str = str(item)
+
+        # Get the expiry date from the database
+        expiry_date = Inventory.query.filter_by(item_name=item_str).first().expiry_date
+
+        # Calculate the days until expiry
+        days_until_expiry = (expiry_date - current_date).days
+
+        # Append the item and days_until_expiry to the expiring items info list
+        expiring_items_info.append({
+            'item': item_str,
+            'days_until_expiry': days_until_expiry
+        })
+
+    # Return the list of expiring items information
+    return jsonify(expiring_items_info)
+
 
 
 @app.route('/add_task', methods=['POST'])
@@ -206,120 +318,89 @@ def register():
 
     return render_template('register.html', form=form)
 
+@app.route('/get_recipes')
+def get_recipes():
+    # Get top 3 expiring items
+    items = Inventory.query.filter_by(user_id=current_user.id).all()
+    expiring_items = get_expiring_items(items)
+    
+    # Get recommended recipes based on expiring items
+    recommended_recipes = get_recipe_recommendations(expiring_items)
+
+    return jsonify(recommended_recipes)
+
 
 @app.route("/predictthedish", methods=["GET"])
 def predictthedish():
     return render_template('Know_Before_You_Eat.html')  
 
 
-@app.route("/predict", methods=["GET", "POST"])
-def upload():
-    data = {}
+@app.route("/predict", methods=["POST"])
+def predict():
     if request.method == "POST":
-        # Get the file from post request
-        f = request.files["image"]
+        data = {}
+        # Get the file from the POST request
+        file = request.files["image"]
 
-        # Save the file to ./upload_image
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, "upload_image", secure_filename(f.filename))
-        f.save(file_path)
+        # Save the file
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(filepath)
 
         # Make prediction
-        image = prepare_image(file_path)
+        image = prepare_image(filepath)
         preds = model.predict(image)
         predictions = preds.argmax(axis=-1)[0]
         pred_label = label_dict[predictions]
 
-        food_retrieve = food_calories[food_calories["name"]==pred_label]
+        # Fetch additional nutritional information from Spoonacular API
+        spoonacular_api_key = "227a4d1c643048d785e6c5401e3f3d81"
+        spoonacular_url = f"https://api.spoonacular.com/recipes/guessNutrition?apiKey={spoonacular_api_key}&title={pred_label}"
+        response = requests.get(spoonacular_url)
+        if response.status_code == 200:
+            spoonacular_data = response.json()
+            print("Spoonacular API Response:", spoonacular_data)  # Debugging print statement
 
-        food_nutrional_min = food_retrieve["nutritional value min,kcal"]
-        food_nutrional_min=np.array(food_nutrional_min)
-        food_nutrional_min = str(food_nutrional_min)
+            # Debugging print statements for nutritional information
+            if 'calories' in spoonacular_data:
+                data['calories'] = spoonacular_data['calories']['value']
+                print("Calories:", data['calories'])  # Debugging print statement
+            else:
+                print("Warning: 'calories' key not found in Spoonacular API response")  # Debugging print statement
 
+            if 'fat' in spoonacular_data:
+                data['fat'] = spoonacular_data['fat']['value']
+                print("Fat:", data['fat'])  # Debugging print statement
+            else:
+                print("Warning: 'fat' key not found in Spoonacular API response")  # Debugging print statement
 
-        food_nutrional_max = food_retrieve["nutritional value max,kcal"]
-        food_nutrional_max=np.array(food_nutrional_max)
-        food_nutrional_max = str(food_nutrional_max)
+            if 'protein' in spoonacular_data:
+                data['protein'] = spoonacular_data['protein']['value']
+                print("Protein:", data['protein'])  # Debugging print statement
+            else:
+                print("Warning: 'protein' key not found in Spoonacular API response")  # Debugging print statement
 
-        Unit = food_retrieve["unit"]
-        Unit=np.array(Unit)
-        Unit = str(Unit)
+            if 'carbs' in spoonacular_data:
+                data['carbs'] = spoonacular_data['carbs']['value']
+                print("Carbs:", data['carbs'])  # Debugging print statement
+            else:
+                print("Warning: 'carbs' key not found in Spoonacular API response")  # Debugging print statement
 
-        Calories = food_retrieve["average cal"]
-        Calories=np.array(Calories)
-        Calories = str(Calories)
+            # Construct response data
+            data = {
+                "pred_label": pred_label,
+                "calories": data.get("calories"),
+                "fat": data.get("fat"),
+                "protein": data.get("protein"),
+                "carbs": data.get("carbs")
+            }
 
-        data = pred_label
-
-        if data=="beef carpaccio":
-           data="carpaccio"
-        elif data=="cheese plate":
-            data="cheese"
-        elif data=="chicken quesadilla":
-            data="quesadilla"
-        elif data=="chicken wings":
-            data="Buffalo wing"
-        elif data=="grilled salmon":
-            data="Salmon#As_food"    
-        elif data=="lobster roll sandwich":
-            data="lobster roll" 
-        elif data=="strawberry shortcake":
-            data="Shortcake#Strawberry_shortcake"
-
-        
-        chrome_driver_path='./chrome-win64/chrome.exe'
-        browser=Browser('chrome',chrome_driver_path,headless=False)
-        # browser=Browser('chrome',path,headless=True)
-
-        if data=="tuna tartare":
-            url="http://ahealthylifeforme.com/tuna-tartare-recipe/"
-            browser.visit(url)
-            html=browser.html
-            soup=BeautifulSoup(html,"html.parser")
-            var=soup.select_one('div.entry-content')
-            description=var.select('p')
+            return jsonify(data)
         else:
-            url="https://en.wikipedia.org/wiki/"
-            browser.visit(url+data)
-            browser.driver.implicitly_wait(10)
-            html=browser.html
-            soup=BeautifulSoup(html,"html.parser")
-            var=soup.select_one('#mw-content-text > div.mw-content-ltr.mw-parser-output')
-            description=var.select('p')
-            nutri=soup.select_one('table.infobox')
+            print("Error: Failed to fetch data from Spoonacular API. Status Code:", response.status_code)  # Debugging print statement
+            return jsonify({"error": "Failed to fetch data from Spoonacular API."}), 500
 
-        if (data=="greek salad" or data=="oysters" or data=="smoked scallop" or data=="paella"):    
-            output=description[1].text
-        elif data=="mussels" :
-            output=description[2].text
-        elif data=="Salmon#As_food":
-            output=description[3].text        
-        else:
-            if description[0].text!='\n':
-                output=description[0].text    
-            elif description[0].text=='\n' and description[1].text!='\n':
-                output=description[1].text
-            elif description[1].text=='\n' and description[2].text!='\n':
-                output=description[2].text
-        output
-        description = output
-        browser.quit()
-
-        
-        return "<center><i><h4>" + pred_label.title()+" </h4></i> "+"<b><h3>Probability</h3></b><h4>"+str(preds.max(axis=-1)[0]) + '\n' + "</h4><br><br><b><h4 class=\"desc\">" +\
-        description + "</h4><br><br>" +\
-        "<div class=\"heading-section\"><h2 class=\"mb-4\"><span>Nutrional Facts</span></h2></div><hr></hr>" + \
-        "<h5><b>Nutrional Value - Min (kcal) &nbsp;: &nbsp;</b>" + food_nutrional_min + '\n' + "<br><br>" + \
-        "<b>Nutrional Value - Max (kcal) &nbsp;: &nbsp;</b>" + food_nutrional_max + '\n' + "<br><br>" + \
-        "<b> Avg Calories &nbsp;: &nbsp;</b>" + Calories + "<br><br>" + \
-        "<b> Unit &nbsp;: &nbsp;</b>" + Unit + '\n' + "</h5></center> <br><br>" + \
-        "<div id=\"Recipe\" class=\"heading-section\"><h2 class=\"mb-4\"><span>Recipe - Cookbook </span></h2></div><hr></hr>" + \
-        str(nutri) 
-        
-
-
-    return None
-
+    return jsonify({"error": "Method not allowed."}), 405
 
 if __name__ == "__main__":
     # Serve the app with gevent
